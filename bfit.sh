1e67bea7505404933f87f9ad904f4dab4ca45717
CLwn <marc95gp@gmail.com>
2020-02-27 18:30:18 +0100

    fix bfit.sh and add rpgp.sh

--- bfit.sh
+++ bfit.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 # Marc Garcia
 # Vacia un fichero pasado por parametro
+
 if [$ # -lt 1 ]; then
-echo "ERROR: Hay que pasar el fichero por parametro." >&2
-exit1
+  echo "ERROR: Hay que pasar el fichero por parametro." >&2
+  exit 1
 fi
 if [$1 = "-h" ]; then
-echo "Uso: ./bfit.sh FICHERO"
-echo "Vacia un fichero pasado por parametro"
-echo - e "Opciones:\n\t -h\tAyuda para utilizar el script."
-echo "Solo necesitamos los permisos necesarios para poder modificar el fichero."
-exit0
+    echo "Uso: ./bfit.sh FICHERO"
+    echo "Vacia un fichero pasado por parametro"
+    echo - e "Opciones:\n\t -h\tAyuda para utilizar el script."
+    echo "Solo necesitamos los permisos necesarios para poder modificar el fichero."
+    exit 0
 fi
-7
+
 if [ - f $1 ]; then
-truncate - s 0$1 2 > /dev/ null
-if [$ ? - ne 0 ]; then
-echo "ERROR: No tenemos permisos para vaciar el fichero." >&2
-exit4
-fi
+  truncate - s 0$1 2 > /dev/ null
+
+  if [$ ? - ne 0 ]; then
+    echo "ERROR: No tenemos permisos para vaciar el fichero." >&2
+    exit 4
+  fi
 elif [ - d $1 ]; then
-echo "ERROR: No podemos vaciar un directorio." >&2
-exit2
+  echo "ERROR: No podemos vaciar un directorio." >&2
+  exit 2
 else
-echo "ERROR: El fichero no existe." >&2
-exit3
+  echo "ERROR: El fichero no existe." >&2
+  exit 3
 fi
-exit0
+exit 0
